8cb14a38bd4f42df355e4ba25e83dbca5e558d20
GHOSTER <125458097+ghosterduhf@users.noreply.github.com>
2025-03-03 06:43:41 -0500

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,16 +1,6 @@
+var http = require('http');
 
-const express = require('express');
-const server = express();
-const port = 3000;
-
-server.all('/', (req, res) => {
-  res.send('👁️ Bot is watching...');
-});
-
-function keepAlive() {
-  server.listen(port, '0.0.0.0', () => {
-    console.log(`🕸️ Keep-alive server running on port ${port}`);
-  });
-}
-
-module.exports = keepAlive;
+http.createServer(function (req, res) {
+  res.write("i'm alive");
+  res.end();
+}).listen(8080);
